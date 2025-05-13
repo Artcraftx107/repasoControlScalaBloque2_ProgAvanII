e839cd6f3426873e413c3f4c04e51e6e69dceeba
Artcraft <arturoao@uma.es>
2025-05-13 18:26:24 +0200

    Updated p4, dk why tf e3 doesnt work and idgaf

--- practicaTema4/src/main/scala/Ejercicio3/Ejericicio3.scala
+++ practicaTema4/src/main/scala/Ejercicio3/Ejericicio3.scala
@@ -1,48 +1,62 @@
 package Ejercicio3
 
-//apartado a
-package object utils {
-  def parallel[A, B](a: => A, b: => B): (A, B) = {
-    var va: A = null.asInstanceOf[A]
-    var vb: B = null.asInstanceOf[B]
-
-    val hebraA = new Thread{
-      override def run(): Unit = {
-        va = a
-      }
-    }
-
-    val hebraB = new Thread{
-      override def run(): Unit = {
-        vb = b
-      }
-    }
-
-    hebraA.start(); hebraB.start()
-    hebraA.join(); hebraB.join()
-
-    (va, vb)
-  }
-}
-
 import scala.util.Random
-import utils._
 
-object E3b extends App{
+object Ejericicio3 extends App {
+  //Apartado a
+  def parallel[A, B](a: => A, b: => B): (A, B) = {
+    @volatile var resA: A = null.asInstanceOf[A]
+    @volatile var resB: B = null.asInstanceOf[B]
+
+    val t1 = new Thread{
+      override def run(): Unit = {
+        resA = a
+      }}
+    val t2 = new Thread{
+      override def run(): Unit = {
+        resB = b
+      }}
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    (resA, resB)
+  }
+
   val lista = List.fill(Random.nextInt(10))(Random.nextBoolean())
-
-  def todosTrue(inic: Int, fin: Int): Boolean = {
-    (inic until fin).forall(lista(_))
-  }
-
-  def todosTrueRec(inic: Int, fin: Int): Boolean = {
-    if(inic >= fin) true
-    else lista(inic) && todosTrueRec(inic+1, fin)
-  }
-
-  val mitad = lista.length / 2
-  val(r1, r2) = parallel(todosTrue(0, mitad), todosTrue(mitad, lista.length))
-
   println(s"Lista: $lista")
-  println(s"Todas True? ${r1 && r2}")
+
+  def todosTrueIter(inic: Int, fin: Int, lista: List[Boolean]): Boolean = {
+    var fard = true
+    for (i <- inic until fin) {
+      if (!lista(i)) {
+        fard = false
+      }
+    }
+    fard
+  }
+
+  private val mitad = lista.length / 2
+
+  val (res1, res2) = parallel(
+    todosTrueIter(0, mitad, lista),
+    todosTrueIter(mitad, lista.length, lista)
+  )
+
+  println("All true?"+(res1&&res2))
+
+  def todosTrueRec(inic: Int, fin: Int, lista: List[Boolean]): Boolean = {
+    if (inic >= fin) {
+      true
+    } else if (!lista(inic)) {
+      false
+    } else {
+      todosTrueRec(inic + 1, fin, lista)
+    }
+  }
+
+
 }
